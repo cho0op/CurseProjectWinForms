1efcbfc6d5362431c432829ce5595e8d40b06f28
Artsiom <sorintema@gmail.com>
2020-05-12 16:55:43 +0300

    registration doesn't work

--- CurseProject/RegisterForm.cs
+++ CurseProject/RegisterForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -45,6 +46,23 @@ namespace CurseProject
                 userNameField.Text = "введите имя";
                 userNameField.ForeColor = Color.Gray;
             }
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            DB db = new DB();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `users` (`login`, `pass`, `name`, `surname`) VALUES ('@login', '@password', '@name', '@surname');", db.GetConnection());
+            String name = loginField.Text;
+            command.Parameters.Add("@login", MySqlDbType.VarChar).Value = "ddd";
+            command.Parameters.Add("@password", MySqlDbType.VarChar).Value = passwordField.Text;
+            command.Parameters.Add("@name", MySqlDbType.VarChar).Value = userNameField.Text;
+            command.Parameters.Add("@surname", MySqlDbType.VarChar).Value = userSurNameField.Text;
+            db.openConnection();
+            if (command.ExecuteNonQuery() == 1)
+                MessageBox.Show("новый пользователь создан");
+            else
+                MessageBox.Show("ошибка создания новго пользователя");
+            db.closeConnection();
         }
     }
 }
